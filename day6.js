a1a9ddf58ac9b7797d72c48968bb79236c2f5640
konwinkler <konrad.winkler@willowtreeapps.com>
2020-12-06 20:18:04 -0500

    day 6 part 2

--- day6.js
+++ day6.js
@@ -1,9 +1,21 @@
 import { readFileSync } from "fs";
 
 const example = `
-BFFFBBFRRR
-FFFBBBFRRR
-BBFFBBFRLL
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
 `;
 
 function readFile(fileName) {
@@ -11,50 +23,46 @@ function readFile(fileName) {
 }
 
 function parse(s) {
-  const lines = s.split("\n").filter(e => e !== "")
-  const boardingPasses = []
-  for(const line of lines) {
-    boardingPasses.push({
-      rowCode: line.slice(0, 7),
-      columnCode: line.slice(7, 10)
-    })
+  const groups = s.split("\n\n").filter(e => e !== "")
+  const data = []
+  for(const group of groups) {
+    const groupData = []
+    const people = group.split("\n").filter(e => e !== "")
+    for(const person of people) {
+      groupData.push(person)
+    }
+    data.push(groupData)
   }
-  return boardingPasses
+  return data
 }
 
-function decode(code) {
-  let result = 0
-  for(let i=0; i < code.length; i++) {
-    const char = code[i]
-    if(char === "B" || char==="R") {
-      result += 2 ** (code.length - i - 1)
+function countEveryone(group) {
+  const answers = new Map()
+  for(const person of group) {
+    for(const question of person) {
+      if(answers.has(question)) {
+        answers.set(question, answers.get(question) + 1)
+      } else {
+        answers.set(question, 1)
+      }
     }
   }
-  return result
+
+  let count = 0
+  for(const answer of answers.keys()) {
+    if(answers.get(answer) === group.length) {
+      count++
+    }
+  }
+  return count
 }
 
-function emptySeat(boardingPasses) {
-  const ids = []
-  for(const boardingPass of boardingPasses) {
-    const row = decode(boardingPass.rowCode)
-    const column = decode(boardingPass.columnCode)
-    console.log(`${row}, ${column}`)
-    ids.push(row * 8 + column)
-  }
-
-  ids.sort((a, b) => (a - b))
-
-  const missing = []
-  for(let i=0; i<ids.length; i++) {
-    const id = ids[i]
-    if(!ids.includes(id + 1) && ids.includes(id + 2)) {
-      missing.push(id + 1)
-    }
-  }
-  return missing
+function combinedAnswers(data) {
+  return data.map(group => countEveryone(group))
+      .reduce((sum, count) => sum + count, 0)
 }
 
 (() => {
-  // console.log(highestID(parse(example)));
-  console.log(emptySeat(parse(readFile("input5.txt"))))
+  console.log(combinedAnswers(parse(example)));
+  console.log(combinedAnswers(parse(readFile("input6.txt"))))
 })();
