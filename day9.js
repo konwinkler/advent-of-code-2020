6f736ea8828abf64dabbb46bb3af4d72321c22db
konwinkler <konrad.winkler@willowtreeapps.com>
2020-12-09 14:07:45 -0500

    day 9 part 2

--- day9.js
+++ day9.js
@@ -75,7 +75,38 @@ const nonXmasNumber = (input, preamble) => {
   return currentNumber
 }
 
+const sumFromRange = (numbers, start, end) => {
+  return numbers.slice(start, end + 1).reduce((sum, e) => sum + e, 0)
+}
+
+const findContiguousList = (input, target) => {
+  // brute force to go through everything
+  for(let i=0; i<input.length; i++) {
+    for(let j = (i + 1); j<input.length; j++) {
+      const sum = sumFromRange(input, i, j)
+      if(target === sum) {
+        return [i, j]
+      }
+    }
+  }
+  throw new Error()
+}
+
+const encryptionWeakness = (input, preamble) => {
+  const invalidNumber = nonXmasNumber(input, preamble)
+
+  // find list of numbers adding up to invalidNumber
+  const [start, end] = findContiguousList(input, invalidNumber)
+  const continuousList = input.slice(start, end + 1)
+
+  // return sum of min and max of this list
+  const min = continuousList.reduce((min, e) => e < min ? e : min, Number.MAX_SAFE_INTEGER)
+  const max = continuousList.reduce((max, e) => e > max ? e : max, Number.MIN_SAFE_INTEGER)
+
+  return min + max
+}
+
 (() => {
-  console.log(nonXmasNumber(parse(example), 5))
-  console.log(nonXmasNumber(parse(readFile("input9.txt")), 25))
+  console.log(encryptionWeakness(parse(example), 5))
+  console.log(encryptionWeakness(parse(readFile("input9.txt")), 25))
 })();
