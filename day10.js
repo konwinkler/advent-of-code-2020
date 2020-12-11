0092c5799d630dca3fcc26bfa62549a30fcbdc06
konwinkler <konrad.winkler@willowtreeapps.com>
2020-12-10 20:37:24 -0500

    day 10 part 2

--- day10.js
+++ day10.js
@@ -1,37 +1,17 @@
 import {readFileSync} from "fs";
 
 const example = `
-28
-33
-18
-42
-31
-14
-46
-20
-48
-47
-24
-23
-49
-45
-19
-38
-39
-11
-1
-32
-25
-35
-8
-17
-7
-9
-4
-2
-34
+16
 10
-3
+15
+5
+1
+11
+7
+19
+6
+12
+4
 `;
 
 const readFile = (fileName) => {
@@ -42,32 +22,47 @@ const parse = (s) => {
   return s.split("\n").filter(e => e !== "").map(Number)
 }
 
-const joltDifferences = (input) => {
-  input.unshift(0)
-  input.sort((a, b) => a - b)
-  input.push(input[input.length - 1] + 3)
-  const steps = input.map((number, index, arr) => {
-    const next = arr[index + 1]
-    return next - number
-  }).filter(e => !isNaN(e))
+const cache = new Map()
 
-  const countSteps = new Map()
-  for(const step of steps) {
-    if(!countSteps.has(step)) {
-      countSteps.set(step, 1)
+const countOptions = (numbers) => {
+  if(numbers.length === 1) {
+    return 1
+  }
+
+  const key = numbers.toString()
+  if(cache.has(key)) {
+    return cache.get((key))
+  }
+
+  // numbers is sorted
+  const first = numbers[0]
+  const options = []
+  for(let i=1; i<4; i++) {
+    const element = numbers[i]
+    if((element !== undefined) && ((element - first) <= 3)) {
+      options.push(i)
     } else {
-      countSteps.set(step, countSteps.get(step) + 1)
+      break
     }
   }
-
-  let total = 1
-  for(const difference of countSteps.keys()) {
-    const count = countSteps.get(difference)
-    total = total * Number(count)
+  let allOptions = 0
+  for(const option of options) {
+    const tail = numbers.slice(option)
+    const tailOptions = countOptions(tail)
+    allOptions += tailOptions
   }
 
-  return total
+  cache.set(key, allOptions)
+  return allOptions
 }
 
-console.log(joltDifferences(parse(example)))
-console.log(joltDifferences(parse(readFile("input10.txt"))))
+const countArrangements = (input) => {
+  input.unshift(0)
+  input.sort((a, b) => a - b)
+  input.push(Math.max(...input) + 3)
+
+  return countOptions(input)
+}
+
+console.log(countArrangements(parse(example)))
+console.log(countArrangements(parse(readFile("input10.txt"))))
