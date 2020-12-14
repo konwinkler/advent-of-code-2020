bb9dc8df7b03ca6c2c59feecb3375c15e55a7988
konwinkler <konrad.winkler@willowtreeapps.com>
2020-12-13 20:27:07 -0500

    day 13 part 2

--- day13.js
+++ day13.js
@@ -6,39 +6,63 @@ const readFile = (fileName) => {
 
 const parse = (s) => {
     const lines = s.split("\n").filter(e => e !== "")
-    const earliestDeparture = Number(lines[0])
     const busIDs = lines[1].split(",")
-        .filter(e => e !== "x")
-        .map(Number)
-    return {
-        earliestDeparture,
-        busIDs
-    }
-}
-
-const departingBus = (notes) => {
-    const {earliestDeparture, busIDs} = notes
-
-    let currentDeparture = earliestDeparture
-    while (true) {
-        for (const busID of busIDs) {
-            const departing = currentDeparture % busID
-            if (departing === 0) {
-                // found departure
-                return {
-                    busID,
-                    time: currentDeparture - earliestDeparture
-                }
-            }
+    const schedule = []
+    for (const [index, bus] of busIDs.entries()) {
+        if (bus !== "x") {
+            schedule.push({
+                busID: Number(bus),
+                time: Number(index)
+            })
         }
-        currentDeparture++
+    }
+    return schedule
+}
+
+function allBusesInOrder(timestamp, schedule) {
+    for (const {time, busID} of schedule) {
+        const departs = (timestamp + time) % busID
+        if (departs !== 0) {
+            return false
+        }
+    }
+    return true
+}
+
+const smallestDivisor = (n) => {
+    if (n % 2 === 0) {
+        return 2
+    }
+    for (let i = 3; i * i <= n; i += 2) {
+        if (n % i === 0) {
+            return i
+        }
+    }
+    return n
+}
+
+const earliestTimestamp = (schedule) => {
+    let increment = 1
+    let start = 0
+    if (schedule.length !== 1) {
+        const laterSchedule = schedule.slice(1)
+        const earlier = earliestTimestamp(laterSchedule)
+        start = earlier
+        // increment is the product of the smallest divisor of each busID
+
+        increment = laterSchedule
+            .map(bus => smallestDivisor(bus.busID))
+            .reduce((product, e) => product * e, 1)
+    }
+
+    let currentTimestamp = start
+    while (true) {
+        if (allBusesInOrder(currentTimestamp, schedule)) {
+            return currentTimestamp
+        }
+        currentTimestamp += increment
     }
 }
 
-const idTimesMinutes = (notes) => {
-    const {busID, time} = departingBus(notes)
-    return busID * time
-}
-
-console.log(idTimesMinutes(parse(readFile("example13.txt"))))
-console.log(idTimesMinutes(parse(readFile("input13.txt"))))
+console.log(earliestTimestamp(parse(readFile("example13.txt"))))
+console.log(earliestTimestamp(parse(readFile("input13.txt"))))
