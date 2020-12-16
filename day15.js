1da263a404dfad56300060c356515ec44c4fb2da
konwinkler <konrad.winkler@willowtreeapps.com>
2020-12-15 20:45:52 -0500

    day 15 part 2

--- day15.js
+++ day15.js
@@ -1,38 +1,32 @@
-function timeBetweenSpoken(sequence, last) {
-    // return if sequence contains only 1 element of last
-    let pointer = sequence.length - 2
-    let time = 1
-    while(pointer >= 0) {
-        const current = sequence[pointer]
-        if(current === last) {
-            return time
-        }
-        time++
-        pointer--
-    }
-
-    return 0
-}
-
 function numberAt(input, target) {
+    const spoken = new Map()
+    let lastSpoken = undefined
     const sequence = input.split(",").map(Number)
 
-    let spoken = [...sequence]
-    let turn = 1 + spoken.length
-    while (turn <= target) {
-        const lastSpoken = spoken[spoken.length - 1]
-        const time = timeBetweenSpoken(spoken, lastSpoken)
-        spoken.push(time)
+    let turn = 1
+
+    while(sequence.length > 0) {
+        if (lastSpoken !== undefined) {
+            spoken.set(lastSpoken, turn - 1)
+        }
+        lastSpoken = sequence.shift()
         turn++
     }
-    return spoken[spoken.length - 1]
+
+    while (turn <= target) {
+        const time = spoken.has(lastSpoken) ? turn - 1 - spoken.get(lastSpoken) : 0
+        spoken.set(lastSpoken, turn - 1)
+        lastSpoken = time
+        turn++
+    }
+    return lastSpoken
 }
 
-console.log(numberAt("0,3,6", 2020))
-console.log(numberAt("1,3,2", 2020))
-console.log(numberAt("2,1,3", 2020))
-console.log(numberAt("1,2,3", 2020))
-console.log(numberAt("2,3,1", 2020))
-console.log(numberAt("3,2,1", 2020))
-console.log(numberAt("3,1,2", 2020))
-console.log(numberAt("0,13,1,8,6,15", 2020))
+console.log(numberAt("0,3,6", 30000000))
+console.log(numberAt("1,3,2", 30000000))
+console.log(numberAt("2,1,3", 30000000))
+console.log(numberAt("1,2,3", 30000000))
+console.log(numberAt("2,3,1", 30000000))
+console.log(numberAt("3,2,1", 30000000))
+console.log(numberAt("3,1,2", 30000000))
+console.log(numberAt("0,13,1,8,6,15", 30000000))
