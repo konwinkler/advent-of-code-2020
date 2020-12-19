3189fa2e0d8b9254c246d906bd6bfd3bee1ae923
konwinkler <konrad.winkler@willowtreeapps.com>
2020-12-18 21:48:28 -0500

    day 18 part 1

---
+++ day18.js
@@ -0,0 +1,98 @@
+import {readFileSync} from "fs";
+
+const readFile = (fileName) => {
+    return readFileSync(fileName, "utf8");
+}
+
+/*
+ * Expression structure
+ * {
+ *  value: number
+ * }
+ * {
+ *  operator: '+'|'*'
+ *  children: [Expression]
+ * }
+ */
+
+function tokenizer(lines) {
+    const all = []
+    for (const line of lines) {
+        const regex = /[\d+*()]/g
+        let token = regex.exec(line)
+        const tokens = []
+        while (token) {
+            let value = token[0]
+            let type
+            if (value === "(" || value === ")") {
+                type = "separator"
+            } else if (value === "*" || value === "+") {
+                type = "operator"
+            } else {
+                type = "number"
+                value = Number(value)
+            }
+            tokens.push({
+                type,
+                value
+            })
+            token = regex.exec(line)
+        }
+        all.push(tokens)
+    }
+    return all
+}
+
+function execute(tokens) {
+    while (tokens.length > 1) {
+        let newTokens
+        let modified = false
+
+        for (let i = 0; !modified && i < tokens.length - 2; i++) {
+            const [beginning, element, end] = tokens.slice(i)
+            if (beginning.value === "(" && end.value === ")") {
+                const result = {
+                    type: "number",
+                    value: element.value
+                }
+                const prefix = tokens.slice(0, i)
+                const suffix = tokens.slice(i + 3)
+                newTokens = [...prefix, result, ...suffix]
+                modified = true
+            }
+        }
+
+        for (let i = 0; !modified && i < tokens.length - 2; i++) {
+            let [first, second, third] = tokens.slice(i)
+            if (first.type === "number" && second.type === "operator" && third.type === "number") {
+                const result = {
+                    type: "number",
+                    value: second.value === "+" ? first.value + third.value : first.value * third.value
+                }
+                const prefix = tokens.slice(0, i)
+                const suffix = tokens.slice(i + 3)
+                newTokens = [...prefix, result, ...suffix]
+                modified = true
+            }
+        }
+
+        tokens = newTokens
+    }
+    return tokens
+}
+
+function parse(fileName) {
+    const file = readFile(fileName)
+    const lines = file.split("\n").filter(e => e !== "")
+    const tokenizedLines = tokenizer(lines)
+
+    const results = []
+    for (const tokens of tokenizedLines) {
+        results.push(execute(tokens))
+    }
+    console.log(results.flat().map(e => e.value))
+
+    return results.flat().map(e => e.value).reduce((sum, e) => sum + e, 0);
+}
+
+console.log(parse("input18.txt"))
